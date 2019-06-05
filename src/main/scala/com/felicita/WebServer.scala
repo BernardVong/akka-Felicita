170e55b935b2c9d99f61575234aa5bb56c2338d1
Manitra RANAIVOHARISON <manitra.harison@gmail.com>
2019-06-05 21:18:39 +0200

    [helper_man]: (updating) -> subscriber Actor

--- src/main/scala/com/felicita/WebServer.scala
+++ src/main/scala/com/felicita/WebServer.scala
@@ -17,49 +17,17 @@ import spray.json.DefaultJsonProtocol._
 import utils.SQLiteHelpers
 import utils.FromMap.to
 
-//#main-class
 object WebServer extends App with SubscriberRoutes {
 
-  // set up ActorSystem and other dependencies here
-  //#main-class
-  //#server-bootstrapping
   implicit val system: ActorSystem = ActorSystem("FelicitaAkkaHttpServer")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val executionContext: ExecutionContext = system.dispatcher
 
-  //#server-bootstrapping
 
   val subscriberRegistryActor: ActorRef = system.actorOf(SubscriberRegistryActor.props, "userRegistryActor")
 
-//  case class Id(id: String)
-//  case class First_name(first_name: String)
-//  case class Last_name(last_name: String)
-//  case class Pseudo(pseudo: String)
-//
-//  case class Keys(vec: Vector[Id])
-//
-//  implicit val keyFormat = jsonFormat1(Id)
-//  implicit val keysFormat = jsonFormat1(Keys)
-  //#main-class
-  // from the UserRoutes trait
-//  val url = s"""jdbc:sqlite:${args(0)}"""
-
-  // lazy val routes: Route = userRoutes
-//  val route: Route =
-//      get {
-//        pathPrefix("subscriber") {
-//          val req = SQLiteHelpers.request(url, "SELECT * FROM table_test", Seq("key"))
-//          req match {
-//            case Some(r) => val values = r.flatMap(v => to[Key].from(v))
-//              complete(values)
-//            case None => complete("mauvaise table")
-//          }
-//        }
-//      }
-//  //#main-class
 lazy val routes: Route = subscriberRoutes
 
-  //#http-server
   val serverBinding: Future[Http.ServerBinding] = Http().bindAndHandle(routes, "localhost", 8080)
 
   serverBinding.onComplete {
@@ -72,8 +40,5 @@ lazy val routes: Route = subscriberRoutes
   }
 
   Await.result(system.whenTerminated, Duration.Inf)
-  //#http-server
-  //#main-class
 }
-//#main-class
-//#quick-start-server
+
--- src/main/scala/com/felicita/actors/SubscriberRegistryActor.scala
+++ src/main/scala/com/felicita/actors/SubscriberRegistryActor.scala
@@ -1,12 +1,22 @@
 package com.felicita
 
 //#user-registry-actor
+import com.typesafe.config.ConfigFactory
+import scala.util.Properties
 import akka.actor.{Actor, ActorLogging, Props}
+import utils.SQLiteHelpers
+import akka.Done
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import spray.json.DefaultJsonProtocol._
+import utils.SQLiteHelpers
+import utils.FromMap.to
 
 //#user-case-classes
 final case class Subscriber(id: String, first_name: String, last_name: String, pseudo: String)
 
-final case class Subscribers(subscribers: Seq[Subscriber])
+final case class Subscribers(subscribers: Vector[Subscriber])
 
 //#user-case-classes
 
@@ -25,10 +35,36 @@ class SubscriberRegistryActor extends Actor with ActorLogging {
 
   var subscribers = Set.empty[Subscriber]
 
+  val myConfig = new MyConfig()
+
   def receive: Receive = {
+
     case GetSubscribers =>
-      sender() ! Subscribers(subscribers.toSeq)
+      val url = myConfig.envOrElseConfig("url")
+      println(s"My secret value is $url")
+
+      val req = SQLiteHelpers.request(url, "SELECT * FROM subscriber", Seq("id","first_name","last_name","pseudo"))
+      println(req)
+      req match {
+        case Some(r) => val values = r.flatMap(s => to[Subscriber].from(s))
+          sender() ! Subscribers(values)
+        case None => complete("mauvaise table")
+      }
+      //sender() ! Subscribers(subscribers.toSeq)
   }
 }
 
+class MyConfig(fileNameOption: Option[String] = None) {
+
+  val config = fileNameOption.fold(
+    ifEmpty = ConfigFactory.load() )(
+    file => ConfigFactory.load(file) )
+
+  def envOrElseConfig(name: String): String = {
+    Properties.envOrElse(
+      name.toUpperCase.replaceAll("""\.""", "_"),
+      config.getString(name)
+    )
+  }
+}
 //#user-registry-actor
